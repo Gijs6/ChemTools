38697a0822d314616f01ad2325e93c37ca8df460
Gijs <79806264+Gijs6@users.noreply.github.com>
2025-02-22 16:08:00 +0100

    Start on stats and move anwnser check to funct

--- saltquiz.py
+++ saltquiz.py
@@ -1,6 +1,8 @@
 import random
 import math
-
+import time
+import statistics
+from statistics import fmean
 
 ions = [
     {
@@ -215,7 +217,6 @@ ions = [
     }
 ]
 
-
 pos_ions = [ion for ion in ions if ion["chargetype"] == "+"]
 neg_ions = [ion for ion in ions if ion["chargetype"] == "-"]
 
@@ -230,7 +231,6 @@ def charges_to_indexes(chargeA, chargeB):
     if amount_B == 1:
         amount_B = ""
     return amount_A, amount_B
-
 
 
 def generate_name_formula_pair():
@@ -261,15 +261,33 @@ def generate_name_formula_pair():
 
     return chemname, formula
 
+
+def check_awnser(input, awnser, capitals):
+    # If capitals == true, the capitals NEED to be checked
+    if capitals:
+        return input.strip() == awnser.strip()
+    else:
+        return input.strip().lower() == awnser.strip().lower()
+
+
+
+
 while True:
     print("Oefenen met zoutformules")
     user_input_amount_questions = input("Hoeveel vragen? ")
     amount_questions = int(user_input_amount_questions) if user_input_amount_questions.isdigit() else 5
     print("\nDaar gaan we!\n")
-    for i in range(amount_questions):
+
+    durations = []
+    correct_answers = 0
+    semicorrect_answers = 0
+    incorrect_answers = 0
+
+    for i in range(1, amount_questions + 1):
         chemname, formula = generate_name_formula_pair()
 
-        chemname_lower = chemname[0].lower() + chemname[1].lower() + chemname[2:]  # The first 2 letters (with the IJ) but not all (with Roman numerals)
+        chemname_lower = chemname[0].lower() + chemname[1].lower() + chemname[
+                                                                     2:]  # The first 2 letters (with the IJ) but not all (with Roman numerals)
         formula_subscript = ''.join('₀₁₂₃₄₅₆₇₈₉'[int(char)] if char.isdigit() else char for char in formula)
 
         if random.choice([0, 0, 1]) == 0:
@@ -277,24 +295,48 @@ while True:
             vraag = f"Geef de formule van {chemname_lower}."
             antwoord = formula
             antwoord_formatted = formula_subscript
+            capitals_setting = True
         else:
             vraag = f"Geef de naam van {formula_subscript}."
             antwoord = chemname_lower
             antwoord_formatted = chemname_lower
+            capitals_setting = False
 
+        print(f"Vraag {i}/{amount_questions}")
         print(vraag)
+        start_time = time.time()
         input_attempt0 = input("Antwoord: ")
 
-        if input_attempt0.strip().lower() == antwoord.strip().lower():
+        if check_awnser(input_attempt0, antwoord.strip(), capitals_setting):
+            end_time = time.time()
+            correct_answers += 1
+
             print("Dat is goed!\n")
         else:
             print("Dat is helaas niet goed.")
             input_attempt1 = input("Probeer het opnieuw: ")
-            if input_attempt1.strip().lower() == antwoord.strip().lower():
+            if check_awnser(input_attempt1, antwoord.strip(), capitals_setting):
+                end_time = time.time()
+                semicorrect_answers += 1
+
                 print("Dat is goed!\n")
             else:
+                end_time = time.time()
+                incorrect_answers += 1
+
                 print("Dat is helaas weer niet goed.")
                 print(f"Het goede antwoord was {antwoord_formatted}\n")
+
+        durations.append(end_time - start_time)
+
+    print("--- Statistieken ---")
+    print(f"Je had {correct_answers} vragen in 1 keer goed ({round(correct_answers / amount_questions * 100, 1)}%).")
+    print(f"Je had {semicorrect_answers} vragen na 1 fout toch goed ({round(semicorrect_answers / amount_questions * 100, 1)}%).")
+    print(f"Je had {incorrect_answers} vragen fout ({round(incorrect_answers / amount_questions * 100, 1)}%).")
+    print(f"Je deed gemiddeld {fmean(durations)} seconden over een vraag.")
+
+
+
     input_again = input("\nWil je nog een keer oefenen? (ja/nee): ")
     if input_again.lower().strip() == "ja":
         print("Nog een keer!")
